8715d4cec5586fd38f18817813ea7ba33db894ab
BELLOJAMIU <jamiub252@gmail.com>
2023-02-23 01:07:26 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,8 +7,8 @@
 
 
 int main(void)
-
 {
+
 	int i;
 
 	printf("infinite loop incoming :(\n");
@@ -18,5 +18,6 @@ int main(void)
 	/*	putchar(i);*/
 	/*}*/
 	printf("infinite loop avoided! \\o/\n");
+
 	return (0);
 }
